a53ad2ca6cea942d05d92575261d850c85f204da
TeacherCelia <theteachercelia@gmail.com>
2025-06-26 08:49:37 +0200

    Initial Commit

---
+++ emma-test-ios/emma_test_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  emma_test_iosApp.swift
+//  emma-test-ios
+//
+//  Created by Celia Pérez Vargas on 26/6/25.
+//
+
+import SwiftUI
+
+@main
+struct emma_test_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
